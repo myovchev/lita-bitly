bf1008cf42b8efdde78732ede76b4254e766941a
Garfield Edgar Carneiro <gary.carneiro@gmail.com>
2014-06-29 22:59:11 +0530

    Lets build a shorten_url() first

--- lib/lita/handlers/bitly.rb
+++ lib/lita/handlers/bitly.rb
@@ -3,7 +3,23 @@ require "url_shortener"
 module Lita
   module Handlers
     class Bitly < Handler
+    
+    
+      #route(/^(?:http|https)(.+)/i,
+      #:shorten, 
+      #command: true
+      #)
+      route(/(?:bitly|shorten)\s(.+)/i,
+      :shorten_url,
+      command: true,
+      help: {"bitly|shorten URL" => "Shorten the URL using bitly"}
+      )
+       
+      def shorten_url()
+        response.reply(response)
+      end
     end
+    
 
     Lita.register_handler(Bitly)
   end
