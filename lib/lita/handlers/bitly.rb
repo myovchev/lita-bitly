3bed0f5008855f5f50446836b19fa0d1d197575f
Garfield Edgar Carneiro <gary.carneiro@gmail.com>
2014-06-30 19:59:10 +0530

    Phew!! Works ; Use ^http://url.subdomain.com  ;   Use  Lita.logger.debug for Logging

--- lib/lita/handlers/bitly.rb
+++ lib/lita/handlers/bitly.rb
@@ -12,11 +12,23 @@ module Lita
       route(/(?:bitly|shorten)\s(.+)/i,
       :shorten_url,
       command: true,
-      help: {"bitly|shorten URL" => "Shorten the URL using bitly"}
+      help: {"bitly | shorten URL" => "Shorten the URL using bitly"}
       )
        
       def shorten_url(response)
-        response.reply(response)
+        inputURL = response.matches[0][0]
+        Lita.logger.debug("Input url -  #{inputURL}")
+        authorize = UrlShortener::Authorize.new 'pcsappops', 'R_1bcac2ff7d3d47f19d667f1b1dcefcf6'
+        Lita.logger.debug(authorize)
+        client = UrlShortener::Client.new authorize
+        Lita.logger.debug(client)
+        shorten = client.shorten(inputURL)
+        Lita.logger.debug("Shorten is #{shorten}")
+        #Lita.logger.debug(shorten.urls)
+        #print UrlShortener::Response::Shorten.class
+        response.reply(shorten.urls)
+        
+       
       end
     end
     
