5d10790c055fa808e00dd87c800a6542a3fb18e1
Garfield Edgar Carneiro <gary.carneiro@gmail.com>
2014-07-05 18:50:57 +0530

    Added config handlers

--- lib/lita/handlers/bitly.rb
+++ lib/lita/handlers/bitly.rb
@@ -14,16 +14,38 @@ module Lita
       command: true,
       help: {"bitly | shorten URL" => "Shorten the URL using bitly"}
       )
-       
+      
+      
+      
+      def self.default_config(handler_config)
+        handler_config.username = nil
+        handler_config.apikey = nil
+      end
+      
       def shorten_url(response)
+        username = Lita.config.handler.bitly.username
+        apikey = Lita.config.handler.bitly.apikey
         inputURL = response.matches[0][0]
-        Lita.logger.debug("Input url -  #{inputURL}")
+        Lita.logger.debug("Bitly() - Input url -  #{inputURL}")
+        
+        if not /^http:\/\/.+/i  =~ inputURL
+            Lita.logger.debug("Bitly() - Input URL Does not start with http://. Appending ..")
+            inputURL.prepend('http://')
+            Lita.logger.debug(inputURL)
+        end
+        
+        
         authorize = UrlShortener::Authorize.new 'pcsappops', 'R_1bcac2ff7d3d47f19d667f1b1dcefcf6'
         Lita.logger.debug(authorize)
         client = UrlShortener::Client.new authorize
         Lita.logger.debug(client)
         shorten = client.shorten(inputURL)
         Lita.logger.debug("Shorten is #{shorten}")
+        
+        #Anything that start with HTTP AND AND AND HTTPS
+        #Use  Exception Handling for Invalid URI and Authentication Exception
+        
+        
         #Lita.logger.debug(shorten.urls)
         #print UrlShortener::Response::Shorten.class
         response.reply(shorten.urls)
